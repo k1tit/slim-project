077b5ac9368cef93478475d0e68c57617fa630bd
k1tit <balashova419@gmail.com>
2024-10-08 18:42:20 +0400

    Программа на PHP

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+// Простой маршрут
+$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+// Запуск приложения
+$app->run();
